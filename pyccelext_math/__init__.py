9b3f2d337e0c2e28c1d3c15c826bbd7c76302823
ratnania <ahmed.ratnani@ipp.mpg.de>
2017-12-07 15:29:50 +0100

    start bsplines

--- pyccelext_math/__init__.py
+++ pyccelext_math/__init__.py
@@ -2,3 +2,4 @@
 __version__ = "0.1"
 
 from .quadratures import *
+from .bsplines    import *
---
+++ pyccelext_math/bsplines.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+#$ header make_knots(int, int)
+def make_knots(n,p):
+    n_elements = n-p
+    m = n+p+1
+    knots = zeros(m, double)
+    for i in range(0, p+1):
+        knots[i] = 0.0
+    for i in range(p+1, n):
+        j = i-p
+        knots[i] = j / n_elements
+    for i in range(n, n+p+1):
+        knots[i] = 1.0
+    return knots
+
+#$ header make_greville(*double [:], int, int)
+def make_greville(knots, n, p):
+    greville = zeros(n, double)
+    for i in range(0, n):
+        s = 0.0
+        for j in range(i+1, i+p+1):
+            s = s + knots[j]
+        greville[i] = s / p
+    return greville
