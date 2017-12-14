dff008822359bad50a33349b5a27f3e664828a92
ratnania <ratnaniahmed@gmail.com>
2017-12-14 22:03:33 +0100

    add unit test for saxpy and daxpy

--- pyccelext_math/testing/external.py
+++ pyccelext_math/testing/external.py
@@ -5,27 +5,48 @@
 from pyccelext.math.external.bsp import spl_compute_greville
 from pyccelext.math.external.bsp import spl_eval_splines_ders
 
-#from pyccelext.math.external.blas import saxpy
-#
-#def test_blas_saxpy():
-#    n = 5
-#    sa = 1.0
-#
-#    incx = 1
-#    sx = zeros(n, double)
-#
-#    incy = 1
-#    sy = zeros(n, double)
-#
-#    sx[0] = 1.0
-#    sx[1] = 3.0
-#    sx[3] = 5.0
-#
-#    sy[0] = 2.0
-#    sy[1] = 4.0
-#    sy[3] = 6.0
-#
-#    saxpy(n, sa, sx, incx, sy, incy)
+from pyccelext.math.external.blas import saxpy
+from pyccelext.math.external.blas import daxpy
+
+def test_blas_saxpy():
+    n = 5
+    sa = 1.0
+
+    incx = 1
+    sx = zeros(n, float)
+
+    incy = 1
+    sy = zeros(n, float)
+
+    sx[0] = 1.0
+    sx[1] = 3.0
+    sx[3] = 5.0
+
+    sy[0] = 2.0
+    sy[1] = 4.0
+    sy[3] = 6.0
+
+    saxpy(n, sa, sx, incx, sy, incy)
+
+def test_blas_daxpy():
+    n = 5
+    sa = 1.0
+
+    incx = 1
+    sx = zeros(n, double)
+
+    incy = 1
+    sy = zeros(n, double)
+
+    sx[0] = 1.0
+    sx[1] = 3.0
+    sx[3] = 5.0
+
+    sy[0] = 2.0
+    sy[1] = 4.0
+    sy[3] = 6.0
+
+    daxpy(n, sa, sx, incx, sy, incy)
 
 def test_greville():
     n_elements = 4
@@ -43,8 +64,6 @@ def test_greville():
 
     g = zeros(n, double)
     g = spl_compute_greville(n, p, knots)
-    print(knots)
-    print(g)
 
 def test_eval_splines():
     n_elements = 4
@@ -76,6 +95,9 @@ def test_eval_splines():
 
     dN = zeros((p1,d1,r1), double)
     dN = spl_eval_splines_ders(p,m,d,r,knots,tau)
+
+test_blas_saxpy()
+test_blas_daxpy()
 
 test_greville()
 test_eval_splines()
